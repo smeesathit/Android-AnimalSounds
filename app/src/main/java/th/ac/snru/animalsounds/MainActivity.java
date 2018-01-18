49e69a619eb3505fe1dbf1012e216a70d1403a7a
smeesathit <smeesathit.gmail.com>
2018-01-18 23:42:01 +0700

    Show toast when tab an image.

--- app/src/main/java/th/ac/snru/animalsounds/MainActivity.java
+++ app/src/main/java/th/ac/snru/animalsounds/MainActivity.java
@@ -1,9 +1,12 @@
 package th.ac.snru.animalsounds;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     // อาร์เรย์ที่เก็บ reference ของ ImageView (รูปภาพสัตว์) ทั้งเก้า
@@ -32,6 +35,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        int resId = 0;
+        int strId = 0;
 
+        switch (v.getId()) {
+            case R.id.bird:
+                resId = R.raw.bird;
+                strId = R.string.bird;
+                break;
+            case R.id.cow:
+                resId = R.raw.cow;
+                strId = R.string.cow;
+                break;
+            case R.id.cat:
+                resId = R.raw.cat;
+                strId = R.string.cat;
+                break;
+            case R.id.dog:
+                resId = R.raw.dog;
+                strId = R.string.dog;
+                break;
+            case R.id.duck:
+                resId = R.raw.duck;
+                    strId = R.string.duck;
+                break;
+
+            case R.id.elephant:
+                resId = R.raw.elephant;
+                strId = R.string.elephant;
+                break;
+            case R.id.horse:
+                resId = R.raw.horse;
+                strId = R.string.horse;
+                break;
+                case R.id.pig:
+                resId = R.raw.pig;
+                strId = R.string.pig;
+                break;
+            case R.id.rooster:
+                resId = R.raw.rooster;
+                strId = R.string.rooster;
+                break;
+        }// End switch
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, strId, duration);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     } // End onClick
 } // End MainActivity
